171e1e1acdef4510d582f5637d0818b3fa8db506
Yushin Washio <yuwash@yandex.com>
2017-01-29 11:27:24 +0100

    gitlab-ci.yml: test routine minor fix

--- EMMA/ci_test.sh
+++ EMMA/ci_test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 LD_LIBRARY_PATH=inc/wiiuse/build/src EMMA/EMMA/release/EMMA
-if [ $? -eq 134 ]
+RET=$?
+if [ $RET -eq 134 ]
 then echo "ci_test.sh: EMMA returned with 134 as expected"
-else return $?
+else exit $RET
 fi
