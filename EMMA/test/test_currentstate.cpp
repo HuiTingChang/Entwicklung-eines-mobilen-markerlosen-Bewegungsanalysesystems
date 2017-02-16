fc6d6c07fc88764b846f8c6e6b9e980950faf7f1
Yushin Washio <yuwash@yandex.com>
2017-02-16 08:36:05 +0100

    test_currentstate: add writeReadTwice [ci skip]

--- EMMA/test/test_currentstate.cpp
+++ EMMA/test/test_currentstate.cpp
@@ -9,6 +9,7 @@ class Test_CurrentState: public QObject
     Q_OBJECT
 private:
     static JointPositions get_example_jpositions();
+    static JointPositions get_modified_example_jpositions(const JointPositions& before);
 signals:
     void write_now();
 private slots:
@@ -16,6 +17,7 @@ private slots:
     void copying();
     void writeReadSeparately();
     void writeRead();
+    void writeReadTwice();
 };
 
 JointPositions Test_CurrentState::get_example_jpositions()
@@ -24,6 +26,17 @@ JointPositions Test_CurrentState::get_example_jpositions()
    SpacePoint p2(2,26,338);
    JointPositions jp {{13,p1},{15,p2}}; 
    return jp;
+}
+
+JointPositions Test_CurrentState::get_modified_example_jpositions(const JointPositions& before)
+{
+    JointPositions after;
+    for(int k = 0; k != before.keys().size(); ++k)
+    {
+	auto asp = before[before.keys()[k]]*1.2;
+	after.insert(before.keys()[k], asp);
+    }
+    return after;
 }
 
 void Test_CurrentState::getters()
@@ -74,6 +87,26 @@ void Test_CurrentState::writeRead()
     emit write_now();
     sio.flush();
     auto siocs = sio.get_reader().next();
+    sio.quit();
+    sio.wait();
+    QCOMPARE(cs, siocs);
+}
+
+void Test_CurrentState::writeReadTwice()
+{
+    CurrentState cs;
+    cs.set_jointPositions(get_example_jpositions());
+    StreamIO sio(&cs);
+    sio.start();
+    connect(this, &Test_CurrentState::write_now, &sio, &StreamIO::write);
+    emit write_now();
+    cs.set_jointPositions(get_modified_example_jpositions(cs.get_joints()));
+    qDebug() << QString(((std::string) cs).c_str());
+    emit write_now();
+    sio.flush();
+    sio.get_reader().next();
+    auto siocs = sio.get_reader().next();
+    qDebug() << QString(((std::string) siocs).c_str());
     sio.quit();
     sio.wait();
     QCOMPARE(cs, siocs);
